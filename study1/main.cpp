28e04de2494f66dd0601f692588069016e3b615c
freshmea <freshmea@naver.com>
2022-09-20 16:51:03 +0900

    mat class point rect rotatedrect point size

--- study1/main.cpp
+++ study1/main.cpp
@@ -9,12 +9,14 @@ int main()
 	cout << "Hello OpenCV " << CV_VERSION << endl;
 
 	Mat img;
-	img = imread("study1/lena.jpg");
+	img = imread("study1/lena.jpg", IMREAD_REDUCED_GRAYSCALE_2);
 
 	if (img.empty()) {
 		cerr << "Image load failed!" << endl;
 		return -1;
 	}
+
+    imwrite("study1/lena_tmp.bmp", img);
 
 	namedWindow("image");
 	imshow("image", img);
